0e86421ef0fdf4187686fc7904e7d346641a4821
zeljkosamed <zeljkosamed@users.noreply.github.com>
2021-08-12 19:10:32 +0300

    angular 2. ders sonu.

---
+++ src/app/components/product/product.component.ts
@@ -0,0 +1,27 @@
+import { ProductService } from './../../services/product.service';
+import { ProductResponseModel } from './../../models/productResponseModel';
+import { Product } from './../../models/product';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css'],
+})
+export class ProductComponent implements OnInit {
+  products: Product[] = [];
+  dataLoaded = false;
+
+  constructor(private productService:ProductService) {}
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts() {
+    this.productService.getProducts().subscribe(response=>{
+      this.products = response.data
+      this.dataLoaded = true;
+    })
+  }
+}
